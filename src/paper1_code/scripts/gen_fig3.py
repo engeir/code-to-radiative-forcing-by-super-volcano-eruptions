16b7df2f791623bf3bea76efdc96f950432ae513
engeir <engeir@pm.me>
2023-11-28 14:16:58 +0100

    docs(README): set up structure

---
+++ src/paper1_code/scripts/gen_fig3.py
@@ -0,0 +1,10 @@
+"""Script that generates plots for figure 3."""
+
+
+def main():
+    """Run the main program."""
+    raise NotImplementedError("Oops, this is not implemented yet.")
+
+
+if __name__ == "__main__":
+    main()
