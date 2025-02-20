fix(figures): use the same normalisation in M20 as in STrop
Signed-off-by: engeir <engeir@pm.me>

src/paper1_code/load/cesm2.py
@@ -3,23 +3,12 @@
 from typing import Literal
 
 import numpy as np
-import scipy
 import xarray as xr
 from volcano_base.load import FindFiles
 
 import paper1_code as core
 
 FINDER = FindFiles()
-
-
-def find_peak(arr: xr.DataArray, version: str) -> float:
-    """Find the peak of an array."""
-    match version:
-        case "savgol":
-            out = scipy.signal.savgol_filter(arr.data, 12, 3).max()
-        case "rolling":
-            out = arr.rolling(time=12, center=True).mean().max().data
-    return out
 
 
 def get_c2w_aod_rf(
@@ -535,11 +524,11 @@ def get_aod_c2w_peaks() -> tuple[float, float, float, float, float]:
     h = core.utils.time_series.get_median(h_, xarray=True)
     v = "rolling"
     return (
-        find_peak(m, version=v),
-        find_peak(mp, version=v),
-        find_peak(s, version=v),
-        find_peak(ss, version=v),
-        find_peak(h, version=v),
+        core.utils.time_series.find_peak(m, version=v),
+        core.utils.time_series.find_peak(mp, version=v),
+        core.utils.time_series.find_peak(s, version=v),
+        core.utils.time_series.find_peak(ss, version=v),
+        core.utils.time_series.find_peak(h, version=v),
     )
 
 
@@ -564,11 +553,11 @@ def get_rf_c2w_peaks() -> tuple[float, float, float, float, float]:
 
     v = "rolling"
     return (
-        find_peak(m, version=v),
-        find_peak(mp, version=v),
-        find_peak(s, version=v),
-        find_peak(ss, version=v),
-        find_peak(h, version=v),
+        core.utils.time_series.find_peak(m, version=v),
+        core.utils.time_series.find_peak(mp, version=v),
+        core.utils.time_series.find_peak(s, version=v),
+        core.utils.time_series.find_peak(ss, version=v),
+        core.utils.time_series.find_peak(h, version=v),
     )
 
 
@@ -592,11 +581,11 @@ def get_rf_coupled_c2w_peaks() -> tuple[float, float, float, float, float]:
     h.data *= -1
     v = "rolling"
     return (
-        find_peak(m, version=v),
-        find_peak(mp, version=v),
-        find_peak(s, version=v),
-        find_peak(ss, version=v),
-        find_peak(h, version=v),
+        core.utils.time_series.find_peak(m, version=v),
+        core.utils.time_series.find_peak(mp, version=v),
+        core.utils.time_series.find_peak(s, version=v),
+        core.utils.time_series.find_peak(ss, version=v),
+        core.utils.time_series.find_peak(h, version=v),
     )
 
 
@@ -620,9 +609,9 @@ def get_trefht_c2w_peaks() -> tuple[float, float, float, float, float]:
     h.data *= -1
     v = "rolling"
     return (
-        find_peak(m, version=v),
-        find_peak(mp, version=v),
-        find_peak(s, version=v),
-        find_peak(ss, version=v),
-        find_peak(h, version=v),
+        core.utils.time_series.find_peak(m, version=v),
+        core.utils.time_series.find_peak(mp, version=v),
+        core.utils.time_series.find_peak(s, version=v),
+        core.utils.time_series.find_peak(ss, version=v),
+        core.utils.time_series.find_peak(h, version=v),
     )

src/paper1_code/load/m20.py
@@ -9,7 +9,6 @@ dataset.
 import datetime
 
 import numpy as np
-import scipy
 import volcano_base
 import xarray as xr
 
@@ -53,20 +52,20 @@ def get_m20(find_all_peaks: bool = False) -> tuple[np.ndarray, ...]:
     time, so2, aod, rf, temp = [], [], [], [], []
     if find_all_peaks:
         for arr in data:
-            # Find peak using savgol filter, then plot SO2 versus {SAOD, ERF, T}.
-            rf_arr = scipy.signal.savgol_filter(
-                arr["effective_radiative_forcing"], 12, 3
+            # Find peak using rolling mean or savgol filter, then plot SO2 versus {SAOD,
+            # ERF, T}.
+            peak_func = core.utils.time_series.find_peak
+            rf_arr = -peak_func(-arr["effective_radiative_forcing"], version="rolling")
+            temp_arr = peak_func(
+                arr["surface_temperature_adjustment"], version="rolling"
             )
-            temp_arr = scipy.signal.savgol_filter(
-                arr["surface_temperature_adjustment"], 12, 3
-            )
-            aod_arr = scipy.signal.savgol_filter(
-                arr["stratospheric_aerosol_optical_depth_at_550_nm"], 12, 3
+            aod_arr = peak_func(
+                arr["stratospheric_aerosol_optical_depth_at_550_nm"], version="rolling"
             )
             so2.append(arr.attrs["SO2 emission (Tg)"])
-            aod.append(aod_arr.max())
-            rf.append(rf_arr.min())
-            temp.append(temp_arr.max())
+            aod.append(aod_arr)
+            rf.append(rf_arr)
+            temp.append(temp_arr)
         return np.asarray(so2), np.asarray(aod), -np.asarray(rf), np.asarray(temp)
     tropical_limit = 10
     for arr in data:

src/paper1_code/scripts/gen_fig1.py
@@ -69,6 +69,82 @@ class DoPlotting:
         self.s_c = core.config.LEGENDS["c2ws"]["c"]
         self.ss_c = core.config.LEGENDS["c2wss"]["c"]
 
+    def _std_shading(
+        self,
+        x_: np.ndarray,
+        sims: list[tuple[float, np.ndarray]],
+        ax_: mpl.axes.Axes,
+        ax1: mpl.axes.Axes,
+        peak_idxs: tuple[int, int, int, int],
+    ) -> None:
+        idm, idp, ids, idss = peak_idxs
+        a = 0.7
+        nth_std = 1
+        mm = np.mean(np.asarray(self.data.medium) / sims[0][0], axis=0)[idm:]
+        ms = np.std(np.asarray(self.data.medium) / sims[0][0], axis=0)[idm:] * nth_std
+        ax_.fill_between(x_, mm - ms, mm + ms, alpha=a, color=self.m_c, ec=None)
+        ax1.fill_between(x_, mm - ms, mm + ms, alpha=a, color=self.m_c, ec=None)
+        pm = np.mean(np.asarray(self.data.plus) / sims[1][0], axis=0)[idp:]
+        ps = np.std(np.asarray(self.data.plus) / sims[1][0], axis=0)[idp:] * nth_std
+        ax_.fill_between(x_, pm - ps, pm + ps, alpha=a, color=self.mp_c, ec=None)
+        ax1.fill_between(x_, pm - ps, pm + ps, alpha=a, color=self.mp_c, ec=None)
+        sm = np.mean(np.asarray(self.data.strong) / sims[2][0], axis=0)[ids:]
+        ss = np.std(np.asarray(self.data.strong) / sims[2][0], axis=0)[ids:] * nth_std
+        ax_.fill_between(x_, sm - ss, sm + ss, alpha=a, color=self.s_c, ec=None)
+        ax1.fill_between(x_, sm - ss, sm + ss, alpha=a, color=self.s_c, ec=None)
+        ssm = np.mean(np.asarray(self.data.superstrong) / sims[3][0], axis=0)[idss:]
+        sss = (
+            np.std(np.asarray(self.data.superstrong) / sims[3][0], axis=0)[idss:]
+            * nth_std
+        )
+        ax_.fill_between(x_, ssm - sss, ssm + sss, alpha=a, color=self.ss_c, ec=None)
+        ax1.fill_between(x_, ssm - sss, ssm + sss, alpha=a, color=self.ss_c, ec=None)
+
+    def _percentile_shading(
+        self,
+        x_: np.ndarray,
+        simulations: list[tuple[float, np.ndarray]],
+        ax_: mpl.axes.Axes,
+        ax1: mpl.axes.Axes,
+        peak_idxs: tuple[int, int, int, int],
+    ) -> None:
+        idm, idp, ids, idss = peak_idxs
+        low = np.linspace(MIN_PERCENTILE, 50, num=1, endpoint=False)
+        high = np.linspace(50, MAX_PERCENTILE, num=1 + 1)[1:]
+        a = 0.7
+        for p1, p2 in zip(
+            np.percentile(self.data.medium, low, axis=0) / simulations[0][0],
+            np.percentile(self.data.medium, high, axis=0) / simulations[0][0],
+            strict=True,
+        ):
+            ax_.fill_between(x_, p1[idm:], p2[idm:], alpha=a, color=self.m_c, ec=None)
+            ax1.fill_between(x_, p1[idm:], p2[idm:], alpha=a, color=self.m_c, ec=None)
+        for p1, p2 in zip(
+            np.percentile(self.data.plus, low, axis=0) / simulations[1][0],
+            np.percentile(self.data.plus, high, axis=0) / simulations[1][0],
+            strict=True,
+        ):
+            ax_.fill_between(x_, p1[idp:], p2[idp:], alpha=a, color=self.mp_c, ec=None)
+            ax1.fill_between(x_, p1[idp:], p2[idp:], alpha=a, color=self.mp_c, ec=None)
+        for p1, p2 in zip(
+            np.percentile(self.data.strong, low, axis=0) / simulations[2][0],
+            np.percentile(self.data.strong, high, axis=0) / simulations[2][0],
+            strict=True,
+        ):
+            ax_.fill_between(x_, p1[ids:], p2[ids:], alpha=a, color=self.s_c, ec=None)
+            ax1.fill_between(x_, p1[ids:], p2[ids:], alpha=a, color=self.s_c, ec=None)
+        for p1, p2 in zip(
+            np.percentile(self.data.superstrong, low, axis=0) / simulations[3][0],
+            np.percentile(self.data.superstrong, high, axis=0) / simulations[3][0],
+            strict=True,
+        ):
+            ax_.fill_between(
+                x_, p1[idss:], p2[idss:], alpha=a, color=self.ss_c, ec=None
+            )
+            ax1.fill_between(
+                x_, p1[idss:], p2[idss:], alpha=a, color=self.ss_c, ec=None
+            )
+
     @overload
     def _plot(
         self,
@@ -94,8 +170,6 @@ class DoPlotting:
         ax: mpl.axes.Axes | None = None,
     ) -> mpl.figure.Figure | mpl.axes.Axes:
         # Percentiles
-        low = np.linspace(MIN_PERCENTILE, 50, num=1, endpoint=False)
-        high = np.linspace(50, MAX_PERCENTILE, num=1 + 1)[1:]
         ax_ = plt.figure().gca() if ax is None else ax
         getattr(ax_, style)()
         ax_.set_xlabel(r"Time after eruption $[\mathrm{yr}]$")
@@ -137,39 +211,8 @@ class DoPlotting:
         plus_scaled = simulations[1][1][idp:]
         strong_scaled = simulations[2][1][ids:]
         superstrong_scaled = simulations[3][1][idss:]
-        a = 0.7
-        for p1, p2 in zip(
-            np.percentile(self.data.medium, low, axis=0) / simulations[0][0],
-            np.percentile(self.data.medium, high, axis=0) / simulations[0][0],
-            strict=True,
-        ):
-            ax_.fill_between(x_, p1[idm:], p2[idm:], alpha=a, color=self.m_c, ec=None)
-            ax1.fill_between(x_, p1[idm:], p2[idm:], alpha=a, color=self.m_c, ec=None)
-        for p1, p2 in zip(
-            np.percentile(self.data.plus, low, axis=0) / simulations[1][0],
-            np.percentile(self.data.plus, high, axis=0) / simulations[1][0],
-            strict=True,
-        ):
-            ax_.fill_between(x_, p1[idp:], p2[idp:], alpha=a, color=self.mp_c, ec=None)
-            ax1.fill_between(x_, p1[idp:], p2[idp:], alpha=a, color=self.mp_c, ec=None)
-        for p1, p2 in zip(
-            np.percentile(self.data.strong, low, axis=0) / simulations[2][0],
-            np.percentile(self.data.strong, high, axis=0) / simulations[2][0],
-            strict=True,
-        ):
-            ax_.fill_between(x_, p1[ids:], p2[ids:], alpha=a, color=self.s_c, ec=None)
-            ax1.fill_between(x_, p1[ids:], p2[ids:], alpha=a, color=self.s_c, ec=None)
-        for p1, p2 in zip(
-            np.percentile(self.data.superstrong, low, axis=0) / simulations[3][0],
-            np.percentile(self.data.superstrong, high, axis=0) / simulations[3][0],
-            strict=True,
-        ):
-            ax_.fill_between(
-                x_, p1[idss:], p2[idss:], alpha=a, color=self.ss_c, ec=None
-            )
-            ax1.fill_between(
-                x_, p1[idss:], p2[idss:], alpha=a, color=self.ss_c, ec=None
-            )
+        # self._percentile_shading(x_, simulations, ax_, ax1, (idm, idp, ids, idss))
+        self._std_shading(x_, simulations, ax_, ax1, (idm, idp, ids, idss))
         ax1.patch.set_alpha(0.3)
         ax1.xaxis.set_major_locator(ticker.MultipleLocator(1))
         ax1.xaxis.set_minor_locator(ticker.MultipleLocator(0.25))
@@ -294,20 +337,20 @@ class DoPlotting:
         if self.version == "aod":
             self._convert_aod()
         # Find median values
-        # medium_med = np.median(self.data.medium, axis=0)
-        # plus_med = np.median(self.data.plus, axis=0)
-        # strong_med = np.median(self.data.strong, axis=0)
-        # superstrong_med = np.median(self.data.superstrong, axis=0)
-        medium_med = core.utils.time_series.get_median(
-            self.data.medium, xarray=True
-        ).data
-        plus_med = core.utils.time_series.get_median(self.data.plus, xarray=True).data
-        strong_med = core.utils.time_series.get_median(
-            self.data.strong, xarray=True
-        ).data
-        superstrong_med = core.utils.time_series.get_median(
-            self.data.superstrong, xarray=True
-        ).data
+        medium_med = np.mean(self.data.medium, axis=0)
+        plus_med = np.mean(self.data.plus, axis=0)
+        strong_med = np.mean(self.data.strong, axis=0)
+        superstrong_med = np.mean(self.data.superstrong, axis=0)
+        # medium_med = core.utils.time_series.get_median(
+        #     self.data.medium, xarray=True
+        # ).data
+        # plus_med = core.utils.time_series.get_median(self.data.plus, xarray=True).data
+        # strong_med = core.utils.time_series.get_median(
+        #     self.data.strong, xarray=True
+        # ).data
+        # superstrong_med = core.utils.time_series.get_median(
+        #     self.data.superstrong, xarray=True
+        # ).data
         extreme = (
             "max"
             if abs(self.data.medium[0].data.min()) < abs(self.data.medium[0].data.max())

src/paper1_code/scripts/gen_fig2.py
@@ -96,7 +96,7 @@ class DoPlotting:
             plot(
                 convert_aod(np.array(self.data.aod_c2w_peak)),
                 np.array(self.data.rf_c2w_peak) * (-1),
-                label="STrop Peaks*",
+                label="C2W Peaks*",
                 c="none",
                 ec="red",
                 lw=1.5,
@@ -194,7 +194,7 @@ def main(show_output: bool = False):
                 unique_labels[lab] = line[i]
     # Reorder
     order = [
-        "STrop Peaks*",
+        "C2W Peaks*",
         "J05 Peak*",
         "T Peak*",
         "P Peak*",

src/paper1_code/utils/time_series.py
@@ -7,6 +7,7 @@ from typing import Literal, overload
 import cftime
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.typing as npt
 import scipy
 import xarray as xr
 
@@ -545,6 +546,20 @@ def weighted_season_avg(da: xr.DataArray) -> xr.DataArray:
     return obs_sum / ones_out
 
 
+def find_peak(arr: xr.DataArray | npt.NDArray, version: str) -> float:
+    """Find the peak of an array."""
+    match version:
+        case "savgol":
+            out = scipy.signal.savgol_filter(arr.data, 12, 3).max()
+        case "rolling":
+            if not isinstance(arr, xr.DataArray):
+                arr = xr.DataArray(
+                    arr, dims="time", coords={"time": np.arange(len(arr))}
+                )
+            out = arr.rolling(time=12, center=True).mean().max().data
+    return out
+
+
 def normalize_peaks(*args: tuple[list | np.ndarray, str]) -> tuple[list, ...]:
     """Normalize the input arrays.
 
@@ -565,7 +580,7 @@ def normalize_peaks(*args: tuple[list | np.ndarray, str]) -> tuple[list, ...]:
         However many tuples with arrays are sent in, as many lists are returned
     """
     out: list[list] = []
-    win_length = 12
+    win_length = 6
     for tup in args:
         arrs = []
         for i in range(len(tup[0])):